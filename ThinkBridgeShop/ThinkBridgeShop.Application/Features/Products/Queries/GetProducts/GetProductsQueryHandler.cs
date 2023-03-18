08c50d394856abd1f621e2703f34545db0ec9afd
Kevin Rivas <krivas.reyes@gmail.com>
2023-03-18 18:01:23 -0400

    Add Get products logic

--- ThinkBridgeShop/ThinkBridgeShop.Application/Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
+++ ThinkBridgeShop/ThinkBridgeShop.Application/Features/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -6,27 +6,23 @@ using ThinkBridgeShop.Application.Features.Products.Commands.DeleteProduct;
 using ThinkBridgeShop.Application.Features.Products.Queries.GetProducts;
 using ThinkBridgeShop.Domain.Dtos;
 using ThinkBridgeShop.Domain.Entities;
+using ThinkBridgeShop.Infrastructure.Interfaces;
 
 namespace ThinkBridgeShop.Application.Features.Products.Queries.GetProducts
 {
     public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<ProductDto>>
     {
-        public async Task<IEnumerable<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        private readonly IRepositoryAsync<Product> _productRepository;
+        private readonly IMapper _mapper;
+        public GetProductsQueryHandler(IRepositoryAsync<Product> productRepository, IMapper mapper)
         {
-            return await Task.Run(() =>
-            {
-                return new List<ProductDto>()
-                {
-                    new ProductDto()
-                    {
-                        Id=1,
-                        Name="Hammer",
-                        Description="Tool",
-                        Price=20
-                     }
-                };
-
-            });
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {   
+           var products=await _productRepository.GetAllAsync();
+            return  _mapper.Map<ProductDto[]>(products); ;
         }
     }
 
