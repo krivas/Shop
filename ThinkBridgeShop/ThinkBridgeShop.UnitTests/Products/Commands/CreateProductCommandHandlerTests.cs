bdd768978c884fe108ed11f158ed37f06d9d7ca9
Kevin Rivas <krivas.reyes@gmail.com>
2023-03-20 01:41:12 -0400

    Add Create product test

--- ThinkBridgeShop/ThinkBridgeShop.UnitTests/Products/Commands/CreateProductCommandHandlerTests.cs
+++ ThinkBridgeShop/ThinkBridgeShop.UnitTests/Products/Commands/CreateProductCommandHandlerTests.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using AutoMapper;
-using Moq;
+
 using ThinkBridgeShop.Application.Contracts.Repositories;
 using ThinkBridgeShop.Application.Features.Products.Commands.CreateProduct;
+using ThinkBridgeShop.Domain.Dtos;
 using ThinkBridgeShop.Domain.Entities;
+using ThinkBridgeShop.UnitTests.Mocks;
 
 namespace ThinkBridgeShop.UnitTests.Products.Commands
 {
@@ -13,11 +14,31 @@ namespace ThinkBridgeShop.UnitTests.Products.Commands
         private readonly Mock<IRepositoryAsync<Product>> _mockProductRepository;
         public CreateProductCommandHandlerTests()
         {
+            _mockProductRepository = ProductsMocksRepository.GetProductRepository();
             var configurationProvider = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile<MappingProfile>();
             });
             _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Test_CreateProduct()
+        {
+            var handler = new CreateProductCommandHandler(_mockProductRepository.Object, _mapper);
+            var createProductCommand = new CreateProductCommand()
+            {
+                Price=30,
+                Name="Hammer",
+                Description="Tool"
+            };
+            var result = await handler.Handle(createProductCommand, CancellationToken.None);
+            var response=await _mockProductRepository.Object.GetAllAsync(1, 9);
+            var product=response.FirstOrDefault(x => x.Price == createProductCommand.Price && x.Name == createProductCommand.Name && x.Description == createProductCommand.Description);
+            product.Price.ShouldBe(createProductCommand.Price);
+            product.Name.ShouldBe(createProductCommand.Name);
+            product.Description.ShouldBe(createProductCommand.Description);
+            response.Count().ShouldBe(9);
         }
     }
 }
--- ThinkBridgeShop/ThinkBridgeShop.UnitTests/Usings.cs
+++ ThinkBridgeShop/ThinkBridgeShop.UnitTests/Usings.cs
@@ -1,3 +1,4 @@
 ﻿global using Xunit;
 global using Moq;
 global using Shouldly;
+global using AutoMapper;
